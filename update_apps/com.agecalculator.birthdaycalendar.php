a0db65bd83bf33425527cff676db5787d2d22768
MegaCreatives <126667455+MegaCreatives@users.noreply.github.com>
2023-03-25 01:21:56 +0000

    Update com.agecalculator.birthdaycalendar.php

--- update_apps/com.agecalculator.birthdaycalendar.php
+++ update_apps/com.agecalculator.birthdaycalendar.php
@@ -1,1 +1,1 @@
-"55,com.agecalculator.birthdaycalendar,1,This version of Age Calculator became out of date on 25 March 2023. Tap DOWNLOAD below to get the latest version from the Google Play Store."
+"1,com.agecalculator.birthdaycalendar,1,This version of Age Calculator became out of date on 25 March 2023. Tap DOWNLOAD below to get the latest version from the Google Play Store."
